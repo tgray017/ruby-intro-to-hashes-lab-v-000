0552625cbed37e15d4b75dbb0699822a5f93e80e
tgray017 <tgray017@gmail.com>
2018-10-05 22:03:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,9 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {},
+	      :reading_railroad => {
+	        "mortgage_value" => "$100"
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
